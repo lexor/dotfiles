8bc044205f5a32a7687828a5a7b60fb1ccabc217
Emre DEĞER <ben@emre.dev>
2021-06-26 20:29:53 +0300

    feat(zsh): update git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,21 +3,25 @@ alias vim="nvim"
 alias cl="clear;clear"
 
 # Git
-alias gc="git commit -S -v -s"
-alias gs="git status -s -b"
-alias gdc="git diff --cached"
-alias gco="git checkout"
-alias gcb="git checkout -b"
-alias glom="git pull origin master --tags"
-alias gloh="git pull origin HEAD --tags"
-alias grom="git rebase origin/master"
-alias gpoh="git push origin HEAD"
-alias gbv="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
-alias nah="git reset --hard;git clean -df;"
-alias undocommit="git reset HEAD~"
-alias undopush="git push -f origin HEAD^:master" # Undo a `git push`
-alias master="git checkout master && git pull"
-alias dev="git checkout develop && git pull"
+alias gc="_git_dbg commit -S -v -s"
+alias gs="_git_dbg status -s -b"
+alias gdc="_git_dbg diff --cached"
+alias gpp='_git_dbg push lexor HEAD'
+alias gpem='_git_dbg push lexor HEAD'
+alias glem='_git_dbg pull lexor HEAD'
+alias gfem='_git_dbg fetch lexor'
+alias gco="_git_dbg checkout"
+alias gcb="_git_dbg checkout -b"
+alias glom="_git_dbg pull origin master --tags"
+alias gloh="_git_dbg pull origin HEAD --tags"
+alias grom="_git_dbg rebase origin/master"
+alias gpoh="_git_dbg push origin HEAD"
+alias gbv="_git_dbg for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+alias nah="_git_dbg reset --hard;git clean -df;"
+alias undocommit="_git_dbg reset HEAD~"
+alias undopush="_git_dbg push -f origin HEAD^:master" # Undo a `git push`
+alias master="_git_dbg checkout master && git pull"
+alias dev="_git_dbg checkout develop && git pull"
 alias lg="lazygit"
 
 # Bat
