45cb49a2ac2fb3ee98016ce86762a741db276195
Emre Deger <ben@emre.dev>
2024-05-11 01:31:47 +0300

    feat(nvim): add lua configs

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+-- these are configured in ./lua/lsp/init.lua
+-- TODO: See if we can move the settings here
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- "zapling/mason-conform.nvim",
+		-- "jayp0521/mason-null-ls.nvim", -- ensure installed null_ls dependencies through Mason
+		"simrat39/rust-tools.nvim",
+	},
+	lazy = true,
+}
